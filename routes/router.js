0b642914b3df6158e36c7b4ea721f3d26ff31aa5
grillopress <grillopress@gmail.com>
2017-10-03 11:53:22 +0100

    update readme with installation instructions

--- routes/router.js
+++ routes/router.js
@@ -11,14 +11,21 @@ router.get('/home', function(req, res, next) {
   res.render( 'index' );
 });
 
-/* GET v1 index page. */
+// // // // // // //
+// v1 Directory Route Code
+//
+// Copy this and replace with v2 etc. to recreate
+// // // // // // //
+
+// GET v1 index page.
 router.get('/v1', function(req, res, next) {
   res.render( 'v1/index' );
 });
 
 // GET all v1 URL reqs and push them to a template in the v1 file
 // This doesn't handle sub-urls yet...
-// This feels really brittle and hacky..
+// This feels really brittle and hacky...
+// No handling of no view found...
 router.get('/v1/:view', function(req, res, next) {
   var theView = req.params.view;
   res.render( 'v1/' + theView );
